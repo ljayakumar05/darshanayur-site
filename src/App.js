bd3b3496ca781ff15b97426c611bd5fa06b7ea4c
Jayakumar L <ljayakumar05@gmail.com>
2018-12-19 19:58:48 +0530

    learn js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import './App.css';
 
 class App extends Component {
   render() {
+      var i = 1;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+          <h1>Header</h1>
+          <h2>Content</h2>
+          <h3>This is the Content..</h3>
+          <h1>{5+5}</h1>
+          <h1>{i == 1 ? 'True!' : 'False'}</h1>
       </div>
     );
   }
